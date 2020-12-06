964141ced0146dd85ebb048ab0503b540a86702f
anonymous-NM-sparsity <melementnonzero4@gmail.com>
2020-12-06 20:44:18 +0800

    res

--- classification/models/resnet.py
+++ classification/models/resnet.py
@@ -18,22 +18,22 @@ __all__ = ['ResNetV1', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
 
 
 
-def conv3x3(in_planes, out_planes, stride=1):
+def conv3x3(in_planes, out_planes, stride=1, N=2, M=4):
     """3x3 convolution with padding"""
     return SparseConv(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+                     padding=1, bias=False, N=N, M=M)
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, N=2, M=4):
         super(BasicBlock, self).__init__()
 
-        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.conv1 = conv3x3(inplanes, planes, stride, N=N, M=M)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
+        self.conv2 = conv3x3(planes, planes, N=N, M=M)
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
@@ -59,15 +59,15 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, N=2, M=4):
         super(Bottleneck, self).__init__()
 
-        self.conv1 = SparseConv(inplanes, planes, kernel_size=1, bias=False)
+        self.conv1 = SparseConv(inplanes, planes, kernel_size=1, bias=False, N=N, M=M)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = SparseConv(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
+                               padding=1, bias=False, N=N, M=M)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = SparseConv(planes, planes * 4, kernel_size=1, bias=False)
+        self.conv3 = SparseConv(planes, planes * 4, kernel_size=1, bias=False, N=N, M=M)
         self.bn3 = nn.BatchNorm2d(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -110,10 +110,10 @@ class ResNetV1(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, layers[0], N = self.N, M = self.M)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, N = self.N, M = self.M)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, N = self.N, M = self.M)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, N = self.N, M = self.M)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -122,20 +122,20 @@ class ResNetV1(nn.Module):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
 
-    def _make_layer(self, block, planes, blocks, stride=1):
+    def _make_layer(self, block, planes, blocks, stride=1, N = 2, M = 4):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 SparseConv(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                          kernel_size=1, stride=stride, bias=False,  N=N, M=M),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(block(self.inplanes, planes, stride, downsample, N=N, M=M))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+            layers.append(block(self.inplanes, planes, N=N, M=M))
 
         return nn.Sequential(*layers)
 
@@ -159,7 +159,7 @@ class ResNetV1(nn.Module):
 
 
 def resnet18(**kwargs):
-    model = ResNetV1(BasicBlock, [2, 2, 2, 2], **kwargs)
+    model = ResNetV1(BasicBlock, [2, 2, 2, 2],  **kwargs)
     return model
 
 
@@ -169,7 +169,7 @@ def resnet34(**kwargs):
 
 
 def resnet50(**kwargs):
-    model = ResNetV1(Bottleneck, [3, 4, 6, 3], **kwargs)
+    model = ResNetV1(Bottleneck, [3, 4, 6, 3],  **kwargs)
     return model
 
 
